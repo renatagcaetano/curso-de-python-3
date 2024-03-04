13d3ff4cc2aaa4aabc0abda36debaf239c058b49
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-03-04 15:31:55 -0300

    Versão 4 e 5

---
+++ fundamentos_projetos/area_circulo_v4.py
@@ -0,0 +1,5 @@
+from math import pi
+
+raio = float(input('Informe o raio da circunferência: '))
+
+print(f'A área do círculo é de {pi * (raio ** 2)}')
---
+++ fundamentos_projetos/area_circulo_v5.py
@@ -0,0 +1,7 @@
+from math import pi
+
+raio = float(input('Informe o raio da circunferência: '))
+
+print(f'A área do círculo é de {pi * (raio ** 2)}')
+
+print('Nome do módulo', __name__)
