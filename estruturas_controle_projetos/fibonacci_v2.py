a200f29bb9a1bf9bd1496a2debe25355b7359088
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-04-09 16:31:50 -0300

    Versão 2

---
+++ estruturas_controle_projetos/fibonacci_v2.py
@@ -0,0 +1,17 @@
+def fibonacci(limite):
+    penultimo = 0
+    ultimo = 1
+
+    print(f'{penultimo},{ultimo}', end=',')
+
+    while ultimo < limite:
+        proximo = penultimo + ultimo
+        
+        print(proximo, end=',')
+        
+        penultimo = ultimo
+        ultimo = proximo
+
+
+if __name__ == '__main__':
+    fibonacci(1000)
