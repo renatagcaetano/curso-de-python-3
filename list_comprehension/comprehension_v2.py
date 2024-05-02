31922e353953435eced08d4a3288bd453c5e72d8
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-05-02 15:55:57 -0300

    List Comprehension 2

---
+++ list_comprehension/comprehension_v2.py
@@ -0,0 +1,8 @@
+dobro_dos_pares = [i * 2 for i in range(10) if i % 2 == 0]
+print(dobro_dos_pares)
+
+dobro_dos_pares = []
+for i in range(10):
+    if i % 2 == 0:
+        dobro_dos_pares.append(i * 2)
+print(dobro_dos_pares)
