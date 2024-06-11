cb2a00bd1f04b2b4be1fe4274acba9515b27975c
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-06-11 17:09:09 -0300

    Versão 1

---
+++ poo/todo_v1.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+
+class Tarefa:
+    def __init__(self, descricao):
+        self.descricao = descricao
+        self.feito = False
+        self.criacao = datetime.now()
+
+    def concluir(self):
+        self.feito = True
+
+    def __str__(self):
+        return self.descricao + (' (Concluída)' if self.feito else '')
+
+
+def main():
+    casa = []
+    casa.append(Tarefa('Passar a roupa'))
+    casa.append(Tarefa('Lavar a louça'))
+
+    [tarefa.concluir() for tarefa in casa if tarefa.descricao == 
+     'Lavar a louça']
+    for tarefa in casa:
+        print(f'- {tarefa}')
+
+
+if __name__ == '__main__':
+    main()
