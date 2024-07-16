198df906528cb79be418be1ace9d21457f0241df
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-07-16 17:54:33 -0300

    Joins

---
+++ banco-relacional/consultarComJoins.sql
@@ -0,0 +1,9 @@
+SELECT * FROM cidades c INNER JOIN prefeitos p ON c.id = p.cidade_id;
+
+SELECT * FROM cidades c LEFT JOIN prefeitos p ON c.id = p.cidade_id;
+
+SELECT * FROM cidades c RIGHT JOIN prefeitos p ON c.id = p.cidade_id;
+
+SELECT * FROM cidades c LEFT JOIN prefeitos p ON c.id = p.cidade_id
+UNION
+SELECT * FROM cidades c RIGHT JOIN prefeitos p ON c.id = p.cidade_id;
