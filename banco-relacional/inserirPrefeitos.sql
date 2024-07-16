a2cbac02673f312919902ed7ad6056abe36475cd
Renata Caetano <78354745+renatagcaetano@users.noreply.github.com>
2024-07-16 17:27:16 -0300

    Inserir prefeitos

---
+++ banco-relacional/inserirPrefeitos.sql
@@ -0,0 +1,6 @@
+INSERT INTO prefeitos (nome, cidade_id) VALUES 
+('Rodrigo Neves', 2),
+('Raquel Lyra', 3),
+('Zenaldo Coutinho', NULL);
+
+INSERT INTO prefeitos (nome, cidade_id) VALUES ('Rafael Greca', NULL);
